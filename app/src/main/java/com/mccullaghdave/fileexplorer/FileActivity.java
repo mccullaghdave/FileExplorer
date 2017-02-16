96a3eac9eb691887a454d2a16f72a7d3d5bf163f
mccullaghdave <mccullaghdave@gmail.com>
2017-02-16 20:51:06 +0000

    Basic logging added

--- app/src/main/java/com/mccullaghdave/fileexplorer/FileActivity.java
+++ app/src/main/java/com/mccullaghdave/fileexplorer/FileActivity.java
@@ -1,13 +1,33 @@
 package com.mccullaghdave.fileexplorer;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class FileActivity extends AppCompatActivity {
-
+ private static final String TAG = FileActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            Log.d(TAG, "onCreate() Restoring previous state xxxxxxxxxxxxxxxxxxxxxxxx");
+        } else {
+            Log.d(TAG, "onCreate() No saved state availablexxxxxxxxxxxxxxxxxxxx");
+            /* initialize app */
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file);
+
     }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
 }
