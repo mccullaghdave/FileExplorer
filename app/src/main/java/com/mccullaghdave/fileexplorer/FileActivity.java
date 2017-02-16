5af17d0f101635c064602fd157e6f2f0ae986d01
David McCullagh <David McCullagh>
2017-02-16 19:38:00 +0000

    Initial commit

---
+++ app/src/main/java/com/mccullaghdave/fileexplorer/FileActivity.java
@@ -0,0 +1,13 @@
+package com.mccullaghdave.fileexplorer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file);
+    }
+}
